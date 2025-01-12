941fa9b32787c61ab868f162b0b44b4c5db20722
Rafael <iwufkawfe@gmail.com>
2025-01-12 11:26:42 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi_rafa_muhammad_aldera/screens/Favorite_Screen.dart';
+import 'package:wisata_candi_rafa_muhammad_aldera/screens/home_screens.dart';
 import 'package:wisata_candi_rafa_muhammad_aldera/screens/profile_screens.dart';
 import 'package:wisata_candi_rafa_muhammad_aldera/screens/search_screens.dart';
 import 'package:wisata_candi_rafa_muhammad_aldera/screens/sign_in_screens.dart';
 import 'package:wisata_candi_rafa_muhammad_aldera/screens/sign_up_screens.dart';
-import 'screens/detail_screens.dart';
-import 'data/candi_data.dart';
 
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -31,11 +36,87 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const MainScreen(),
+      initialRoute: '/',
+      routes: {
+        '/homescreen': (context) => const HomeScreen(),
+        '/signin' : (context) => const SignInScreen(),
+        '/signup' : (context) => SignUpScreen(),
+      },
       //home: ProfileScreen(),
       //home: DetailScreen(candi: candiList[0]),
       //home: SignInScreen(),
       //home: SignUpScreen(),
-      home: SearchScreen(),
+      //home: SearchScreen(),
+      //home: HomeScreen(),
     );
   }
 }
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //Todo: 1.Deklarasi variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Todo: 2.body
+      body: _children[_currentIndex],
+      //todo: 3.BottomNavigatorBar
+      bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple[50]),
+          child: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex=index;
+              });
+            },
+            //Todo: 4.data child theme
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Search"),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Favorite"),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.deepPurple,
+                  ),
+                  label: "Person"),
+            ],
+            selectedItemColor: Colors.deepPurple,
+            unselectedItemColor: Colors.deepPurple[100],
+            showSelectedLabels: true,
+          )),
+    );
+  }
+}
+
--- lib/screens/profile_screens.dart
+++ lib/screens/profile_screens.dart
@@ -1,19 +1,47 @@
+import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
-// TODO:1.mendeklasrasikan variable yang di butuhkan
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  bool isSignedIn = true;
-  String fullName = "";
-  String userName = "";
+  // TODO: 1.Deklarasikan variabel yang dibutuhkan
+  bool isSignedIn = false;
+  String fullName = '';
+  String userName = '';
   int favoriteCandiCount = 0;
 
+  Future<Map<String, String>> _retrieveAndDecryptDataFromPrefs(
+      Future<SharedPreferences> prefs)
+  async{
+    final sharedPreferences = await prefs;
+    final encryptedUsername = sharedPreferences.getString('username')?? '';
+    final encryptedPassword = sharedPreferences.getString('password')?? '';
+    final keyString = sharedPreferences.getString('key')?? '';
+    final ivString = sharedPreferences.getString('iv')?? '';
+    final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+    final iv = encrypt.IV.fromBase64(ivString);
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final decrytedUsername = encrypter.decrypt64(encryptedUsername, iv: iv);
+    final decryptedPassword = encrypter.decrypt64(encryptedPassword, iv: iv);
+    //mengembalikan data terdekripsi
+    return{'username': decrytedUsername, 'password':decryptedPassword};
+  }
+  // TODO 5. Implementasi fungsi signIn
+  void signIn(){
+    Navigator.pushNamed(context, '/signin');
+  }
+  // TODO 6. Implementasi fungsi signOut
+  void signOut(){
+    setState(() {
+      isSignedIn = false;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +56,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                //Todo 2.Buat bagian ProfilHeader yang berisi gambar profil
+                //TODO: 2. Buat bagian ProfileHeader yang berisi gambar profil
                 Align(
                   alignment: Alignment.topCenter,
                   child: Padding(
@@ -38,73 +66,110 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       children: [
                         Container(
                           decoration: BoxDecoration(
-                            border:
-                                Border.all(color: Colors.deepPurple, width: 2),
+                            border: Border.all(color: Colors.deepPurple, width: 2),
                             shape: BoxShape.circle,
                           ),
                           child: CircleAvatar(
                             radius: 50,
                             backgroundImage:
-                                AssetImage('images/placeholder_image.png'),
+                            AssetImage('images/placeholder_image.png'),
                           ),
                         ),
                         if (isSignedIn)
                           IconButton(
                               onPressed: () {},
-                              icon: Icon(
-                                Icons.camera_alt,
-                                color: Colors.deepPurple[50],
-                              ))
+                              icon: Icon(Icons.camera_alt,
+                                  color: Colors.deepPurple[50]))
                       ],
                     ),
                   ),
                 ),
-
-                //Todo 3.Buat bagian profilInfo yang berisi info Profil
-                SizedBox(
-                  height: 10,
-                ),
-                Divider(
-                  color: Colors.deepPurple[100],
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-
+                //TODO: 3. Buat bagian ProfileInfo yang berisi info profil
+                // Baris Pengguna
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
                 Row(
                   children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 3,
+                    SizedBox(width: MediaQuery.of(context).size.width / 3,
                       child: Row(
                         children: [
-                          Icon(
-                            Icons.lock,
-                            color: Colors.amber,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            'Pengguna',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
+                          Icon(Icons.lock, color: Colors.amber),
+                          SizedBox(width: 8),
+                          Text('Pengguna',style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)
                           )
                         ],
                       ),
                     ),
                     Expanded(
-                        child: Text(
-                      ': $userName',
-                      style: TextStyle(fontSize: 18),
-                    ))
-                    //expand berguna untuk memberi jarak pada widget selanjutnya
+                      child: Text(': $userName', style: TextStyle(
+                          fontSize: 18),
+                      ),
+                    ),
+                    if(isSignedIn) Icon(Icons.edit),
                   ],
-                )
+                ),
+                // Baris Nama
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 3,
+                      child: Row(
+                        children: [
+                          Icon(Icons.person, color: Colors.blue),
+                          SizedBox(width: 8),
+                          Text('Nama',style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(': $fullName', style: TextStyle(
+                          fontSize: 18),
+                      ),
+                    ),
+                    if(isSignedIn) Icon(Icons.edit),
+                  ],
+                ),
+                // Baris Favorit
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 4),
+                Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width / 3,
+                      child: Row(
+                        children: [
+                          Icon(Icons.favorite, color: Colors.red),
+                          SizedBox(width: 8),
+                          Text('Favorit',style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(': $favoriteCandiCount', style: TextStyle(
+                          fontSize: 18),
+                      ),
+                    ),
+                    if(isSignedIn) Icon(Icons.edit),
+                  ],
+                ),
 
-                //Todo 4.Buat ProfilActions yang berisi textbutton sign in/out
+                //TODO: 4. Buat ProfileActions yang berisi TextButton sign in/out
+                SizedBox(height: 4),
+                Divider(color: Colors.deepPurple[100]),
+                SizedBox(height: 20),
+                isSignedIn ? TextButton(onPressed: signOut, child: Text('Sign Out'))
+                    : TextButton(onPressed: signIn, child: Text('Sign In')),
               ],
             ),
-          ),
+          )
         ],
       ),
     );
